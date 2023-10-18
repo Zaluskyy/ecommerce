cc63239e6796eadadbea24b2354ae75fd7041278
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2023-10-18 11:37:03 +0200

    added google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import NavBar from './components/NavBar'
 import { EcommerceContextProvider } from './store/context'
-import Head from 'next/head'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '900'] })
 
 export const metadata = {
   title: 'Zaluskyy shop',
   description: 'The best shop',
+  verification: {
+    google: 'tRsEUBkFRrQ_dehZlRf-uSYPHdQZ5KUri1KPBPRYiP0',
+  }
 }
 
 export default function RootLayout({
@@ -18,9 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="tRsEUBkFRrQ_dehZlRf-uSYPHdQZ5KUri1KPBPRYiP0" />
-      </Head>
       <EcommerceContextProvider>
         <body className={poppins.className}>
           <NavBar/>
