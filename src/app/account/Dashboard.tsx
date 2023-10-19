bc13022feecd0cba9ce0f65fe4022773a88f3f6a
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2023-10-19 18:46:41 +0200

    Logout added

--- src/app/account/Dashboard.tsx
+++ src/app/account/Dashboard.tsx
@@ -1,10 +1,19 @@
-import React, { SetStateAction } from 'react'
+import React, { useContext } from 'react'
 import style from './style/Dashboard.module.scss';
 import Image from 'next/image';
 
 import ordersIcon from '../../../public/img/icon/orders.svg'
 import settingsIcon from '../../../public/img/icon/settings.svg'
 import arrowIcon from '../../../public/img/icon/arrow.svg'
+import logoutIcon from '../../../public/img/icon/logout.svg'
+
+import { useCookies } from 'react-cookie';
+
+import toast from 'react-hot-toast';
+
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase';
+import EcommerceContext from '../store/context';
 
 
 interface DashboardProps{
@@ -13,20 +22,41 @@ interface DashboardProps{
 
 const Dashboard: React.FC<DashboardProps> = ({setAccountDashboard}) => {
 
+    const context = useContext(EcommerceContext)
+    const { setIsAuth } = context
+
+    const [,, removeCookie] = useCookies(['auth-token']);
+
     interface IDashboardArr{
         icon: string,
         name: string,
         goTo: string,
+        logOut?: boolean,
     }
 
     const dashboardArr: IDashboardArr[] = [
         {icon: ordersIcon, name: "Orders", goTo: "ORDERS"},
         {icon: settingsIcon, name: "Account settings", goTo: "SETTINGS"},
+        {icon: logoutIcon, name: "Logout", goTo: "SETTINGS", logOut: true},
     ]
+
+    const handleSignOut = async ()=>{
+        await signOut(auth)
+        removeCookie("auth-token")
+        setIsAuth(false)
+        toast.success("Logged out")
+      }
+
+    const handleGoTo = (goTo: string, logout: boolean)=>{
+        if(!logout) setAccountDashboard(goTo as "ORDERS"|"SETTINGS")
+        else {
+            handleSignOut()
+        }
+    }
 
     const dashboard = dashboardArr.map(item=>{
         return(
-            <div key={item.name} className={style.element} onClick={()=>setAccountDashboard(item.goTo as "ORDERS"|"SETTINGS")} >
+            <div key={item.name} className={style.element} onClick={()=>handleGoTo(item.goTo, item.logOut?item.logOut:false)}>
                 <div className={style.informationContainer}>
                     <div className={style.imageContainer}>
                         <Image 
